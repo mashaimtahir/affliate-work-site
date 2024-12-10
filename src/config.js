58a1cec27551dcd020f38f0e3ce73201b3b5d04e
Mashaim Tahir <57322309+mashaimtahir@users.noreply.github.com>
2024-12-10 16:22:25 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const Axios = axios.create({
-  baseURL: "https://testing-backend-azure.vercel.app/",
+  baseURL: "https://testing-backend-rose.vercel.app/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
